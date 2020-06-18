7a7614c11db9c9598863e688d07893fbc7bce2b7
Wouter Korstanje <wouterkorstanje@gmail.com>
2020-06-18 20:21:09 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+# Initial Software
+
+sudo apt update
+
+sudo apt install net-tools htop lame git flatpak \
+nano ubuntu-restricted-extras gnome-tweaks \
+gnome-tweak-tool qt5-style-plugins -yy
+
+
+# Remove undesirable packages:
+
+sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -yy
+
+# Remove snaps and get packages from apt:
+
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
+sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
+gnome-software-plugin-flatpak -yy
+
+# Purge Firefox, install Chromium:
+
+sudo apt purge firefox -yy
+sudo apt purge firefox-locale-en -yy
+if [ -d "/home/$USER/.mozilla" ]; then
+    rm -rf /home/$USER/.mozilla
+fi
+if [ -d "/home/$USER/.cache/mozilla" ]; then
+    rm -rf /home/$USER/.cache/mozilla
+fi
+
+sudo apt install chromium-browser
+
+gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
+#set icons to minimize on click
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+
+## Remove junk
+sudo apt-get remove ubuntu-web-launchers thunderbird rhythmbox -y
+
+## Multimedia
+sudo apt-get install -y gimp
+
+## Games
+sudo apt-get install -y steam-installer
+
+## Music
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+## Text Editor
+sudo snap install --classic code
+
+## Disable Apport
+sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+
+# Gotta reboot now:
+sudo apt update && sudo apt upgrade -y
+
+echo $'\n'$"*** All done! Please reboot now. ***"
